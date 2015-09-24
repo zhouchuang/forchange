432e4789e231103c305984a150829e36c5d7445e
kodo <524580860@qq.com>
2015-09-24 20:25:32 +0800

    修改手势构造函数，暂支持tap事件，支持事件代理,支持批量绑定

--- touch/tap1.0.js
+++ touch/tap1.0.js
@@ -1,12 +1,20 @@
 /**
  *  @description 手势构造函数，暂支持tap事件，支持事件代理
  *  @param {String} id
- *  @param {String} selector 支持复杂选择器
+ *  @param {String} selector(可选)  支持复杂选择器
  *  @param {function} fn
- *  @example
+ *  @example  事件代理例子 !!!!!第一个参数仅支持id!!!!!!
+ *
  *  touchModule('#box','li',function(e,touchObj) {
  *		console.log(this,e,touchObj);
  *	})
+ *
+ * @example  直接事件绑定
+ *
+ *  touchModule('#box',function(e,touchObj) {
+ *		console.log(this,e,touchObj);
+ *	})
+ *
  *  e为事件对象，touchObj为触摸返回对象
  */
 (function(window, undefined) {
@@ -35,7 +43,6 @@
 			if (time < 150 && Math.abs(touchObj.distanceX) < 2 && Math.abs(touchObj.distanceY) < 2) {
 				isTap = true;
 				if (isTap) {
-					console.log('tap啦啦啦啦啦～～～');
 					touchObj.distanceX = 0;
 					touchObj.distanceY = 0;
 					//返二个参数 指向被触发的dom，和当前构造函数
@@ -45,7 +52,6 @@
 					}, 30);
 				}
 			} else { //否则为滑动或者双击
-				console.log('我被移动了，不是tap了');
 				touchObj.distanceX = 0;
 				touchObj.distanceY = 0;
 			}
@@ -55,28 +61,41 @@
 	//事件代理用的函数
 	function delegate(agent, type, selector, fn) {
 			//为了复杂的选择器实现
-			agent.addEventListener(type, function(e) {
+			if (typeof selector != "string") {
+				for (var i = 0; i < agent.length; i++) {
+					agent[i].addEventListener(type, fn, false);
+				}
+				return;
+			}
+			agent[0].addEventListener(type, function(e) {
 				var target = e.target;
 				var ctarget = e.currentTarget;
 				do {
-					filiter(agent, selector, target, e);
+					if (filiter(agent, selector, target, e)) {
+						fn.call(target, e); //要吧事件e返回出去调用
+					};
 					target = target.parentNode;
 				} while (target != ctarget)
 			}, false);
 
 			function filiter(agent, selector, target, e) {
-				var nodes = agent.querySelectorAll(selector);
+				var nodes = agent[0].querySelectorAll(selector);
 				for (var i = 0; i < nodes.length; i++) {
 					if (nodes[i] == target) {
-						fn.call(nodes[i], e); //要吧事件e返回出去调用
+						return true;
 					}
 				}
 			}
 		}
 		//事件代理用的函数结束
 
-	function Touchevent(id, selector, fn) {
-		this.dom = document.getElementById(id.substr(1)); //当前的dom
+	function Touchevent(root, selector, fn) {
+		this.root = document.querySelectorAll(root); //当前的dom
+
+		if (!this.root) {
+			console.log('root不存在');
+			return;
+		}
 		this.touchObj = {
 			pageX: 0,
 			pageY: 0,
@@ -88,8 +107,6 @@
 		this.isTap = false; //用来判断是否为tap
 		this.time = 0; //记录点击的时间间隔 
 		this.selector = selector;
-		console.log(typeof id);
-		console.log(arguments);
 		if (arguments[2] == undefined) {
 			this.operate(arguments[1]);
 		} else {
@@ -100,22 +117,17 @@
 		var touchObj = this.touchObj, //缓存touchObj
 			isTap = this.isTap,
 			_this = this;
-		delegate(this.dom, 'touchstart', this.selector, function(e) {
+		delegate(this.root, 'touchstart', this.selector, function(e) {
 			touchStart(e, touchObj, _this)
 		});
-		delegate(this.dom, 'touchmove', this.selector, function(e) {
+		delegate(this.root, 'touchmove', this.selector, function(e) {
 			touchMove(e, touchObj);
 		});
-		delegate(this.dom, 'touchend', this.selector, function(e) {
+		delegate(this.root, 'touchend', this.selector, function(e) {
 			touchEnd(e, this, touchObj, _this, fn);
 		});
 	}
-
-	Touchevent.prototype.on = function(dom, selector, fn) {
-
-	}
-	window.touchModule = function(dom, selector, fn) {
-		return new Touchevent(dom, selector, fn);
+	window.touchModule = function(root, selector, fn) {
+		return new Touchevent(root, selector, fn);
 	};
-
 })(window, undefined);
