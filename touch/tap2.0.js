e61dcece9af5a491635e9543909d815543f3e526
kodo <524580860@qq.com>
2015-09-28 14:22:44 +0800

    修改了tap2.0

--- touch/tap2.0.js
+++ touch/tap2.0.js
@@ -26,7 +26,7 @@
  */
 (function(window, undefined) {
 	function swipeDirection(x1, x2, y1, y2) {
-			return Math.abs(x1 - x2) >= Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down');
+			return Math.abs(x1 - x2) >= Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'left' : 'right') : (y1 - y2 > 0 ? 'up' : 'down');
 		}
 		//事件代理用的函数
 
